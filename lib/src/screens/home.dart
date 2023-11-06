5bc35cb60b2242304082e04f9a48050012c78cbd
Sirius <pranav474738@gmail.com>
2023-11-06 19:11:01 +0530

    final commit

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,11 +5,10 @@ import 'package:http/http.dart' as http;
 import '../constants/constants.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
 
-final dataProvider = FutureProvider<dynamic>((ref) async {
-  var response = await http.get(Uri.parse("${using}"));
-  var data = jsonDecode(response.body);
-  return data;
+final dataProvider = StateProvider<dynamic>((ref) {
+  return;
 });
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -40,122 +39,166 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final data = ref.watch(dataProvider);
     return Container(
-      decoration: BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/test.jpg"),
             fit: BoxFit.cover,
           ),
         ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to HTR"),
-        ),
-        body: data.when(
-          loading: () {
-            return Center(
-              child: const CircularProgressIndicator.adaptive(),
-            );
-          },
-          error: (error, stackTrace) {
-            return const SizedBox();
-          },
-          data: (data) {
-            return Center(
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text("Welcome to HTR"),
+            ),
+            body: Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                ElevatedButton(
-                    onPressed: () async {
-                      await getCamera();
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text("Your Image"),
-                            content: Image.file(
-                              widget._image!,
-                            ),
-                            actions: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                    showAdaptiveDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return AlertDialog(
-                                          title: Text("Converted Text"),
-                                          content:
-                                              Text(data["result"].toString()),
-                                          actions: [
-                                            ElevatedButton(
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: Text("Ok"))
-                                          ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () async {
+                          await getCamera();
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Your Image"),
+                                content: Image.file(
+                                  widget._image!,
+                                ),
+                                actions: [
+                                  ElevatedButton(
+                                      onPressed: () async {
+                                        final bytes =
+                                            widget._image!.readAsBytesSync();
+                                        String img64 = base64Encode(bytes);
+                                        var post_data = {
+                                          "img_data": img64.toString()
+                                        };
+                                        var response = await http.post(
+                                            Uri.parse(using + "/"),
+                                            body: post_data);
+
+                                        var data = jsonDecode(
+                                            response.body.toString());
+                                        // ref.watch(dataProvider.notifier).state =
+                                        //     data;
+                                        Navigator.of(context).pop();
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text("Converted Text"),
+                                              content: Text(
+                                                  data["result"].toString()),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    child: Text("Ok")),
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Clipboard.setData(new ClipboardData(text: data.toString()));
+                                                      SnackBar snackBar = SnackBar(
+                                                          content: Text(
+                                                              "Text has been copied!"));
+                                                      ScaffoldMessenger.of(
+                                                              context)
+                                                          .showSnackBar(
+                                                              snackBar);
+                                                    },
+                                                    child: Text("Copy"))
+                                              ],
+                                            );
+                                          },
                                         );
                                       },
-                                    );
-                                  },
-                                  child: Text("Convert"))
-                            ],
+                                      child: Text("Convert"))
+                                ],
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    child: Text(" Scan  " , style: TextStyle(
-                      fontSize: 40
-                    ),)),
-                    const SizedBox(height: 40,),
-                ElevatedButton(
-                    onPressed: () async {
-                      await pickImage();
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text("Your Image"),
-                            content: Image.file(
-                              widget._image!,
-                            ),
-                            actions: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                    showAdaptiveDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return AlertDialog(
-                                          title: Text("Converted Text"),
-                                          content:
-                                              Text(data["result"].toString()),
-                                          actions: [
-                                            ElevatedButton(
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: Text("Ok"))
-                                          ],
+                        child: Text(
+                          " Scan  ",
+                          style: TextStyle(fontSize: 40),
+                        )),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          await pickImage();
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Your Image"),
+                                content: Image.file(
+                                  widget._image!,
+                                ),
+                                actions: [
+                                  ElevatedButton(
+                                      onPressed: () async {
+                                        final bytes =
+                                            widget._image!.readAsBytesSync();
+                                        String img64 = base64Encode(bytes);
+                                        var post_data = {
+                                          "img_data": img64.toString()
+                                        };
+                                        var response = await http.post(
+                                            Uri.parse(using + "/"),
+                                            body: post_data);
+                                        var data = jsonDecode(
+                                            response.body.toString());
+                                        // ref.watch(dataProvider.notifier).state =
+                                        //     data;
+                                        Navigator.of(context).pop();
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text("Converted Text"),
+                                              content: Text(
+                                                  data["result"].toString()),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    child: Text("Ok")),
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Clipboard.setData(new ClipboardData(text: data.toString()));
+                                                      SnackBar snackBar = SnackBar(
+                                                          content: Text(
+                                                              "Text has been copied!"));
+                                                      ScaffoldMessenger.of(
+                                                              context)
+                                                          .showSnackBar(
+                                                              snackBar);
+                                                    },
+                                                    child: Text("Copy"))
+                                              ],
+                                            );
+                                          },
                                         );
                                       },
-                                    );
-                                  },
-                                  child: Text("Convert"))
-                            ],
+                                      child: Text("Convert"))
+                                ],
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    child: Text("Upload" , style: TextStyle(
-                      fontSize: 40,
-                    ),)),
-              ]),
-            );
-          },
-        ),
-      ),
-    );
+                        child: Text(
+                          "Upload",
+                          style: TextStyle(
+                            fontSize: 40,
+                          ),
+                        )),
+                  ]),
+            )));
   }
 }
